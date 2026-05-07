3148a0d48e2a55c01ad5dabc35882e43ee727345
eJunk Studios <firstnamedotlastname42@gmail.com>
2026-05-07 03:51:57 -0500

    Update Big-Black-Gym-Log-Test.js

--- BBGL Test Branch/Big-Black-Gym-Log-Test.js
+++ BBGL Test Branch/Big-Black-Gym-Log-Test.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Big Black Gym Log Teste
 // @namespace    http://tampermonkey.net/
-// @version      0.9.28
+// @version      0.9.29
 // @description  A high-fidelity, gamified stat tracker built to integrate seamlessly with Torn's native UI.
 // @author       BigBlackHawk [3550896]
 // @match        https://www.torn.com/*
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/BigBlackHawk42069/Big-Black-Gym-Log/refs/heads/main/BBGL%20Test%20Branch/Big-Black-Gym-Log-Test.js
 // @downloadURL  https://raw.githubusercontent.com/BigBlackHawk42069/Big-Black-Gym-Log/refs/heads/main/BBGL%20Test%20Branch/Big-Black-Gym-Log-Test.js
+// @run-at       document-body
 // ==/UserScript==
 
 (function () {
